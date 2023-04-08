d297a7cc1219dd414af8666ca759b57c2eac178d
djbokisha <bojan.bokisha@gmail.com>
2023-04-08 02:28:41 +0200

    navbar route log in/out

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
 import logo from "../../assets/AiLogo.png";
 import "./Navbar.css";
-import hamburger from "../../assets/hamburger.svg"
-import hamburger_close from "../../assets/hamburger-close.svg"
+import hamburger from "../../assets/hamburger.svg";
+import hamburger_close from "../../assets/hamburger-close.svg";
 import { useNavigate } from "react-router-dom";
-import { useLocalStorage } from "../../hooks/useLocalStorage"
+import { useLocalStorage } from "../../hooks/useLocalStorage";
+import { useAuth } from "../../hooks/useAuth";
 
 function Navbar() {
   const [click, setClick] = useState(false);
@@ -12,49 +13,72 @@ function Navbar() {
   const hadleClick = () => setClick(!click);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  const { getItem } = useLocalStorage();
+  const { logout } = useAuth();
 
+  const Logout = () => {
+    logout();
+    navigate("/login");
+    hadleClick();
+  };
 
-  // const { getItem } = useLocalStorage();
+  useEffect(() => {
+    const user = getItem("user");
+    setIsLoggedIn(user ? true : false);
+  }, [getItem]);
 
-  // const [user, setUser] = useState([]);
-  // useEffect(() => {
-  //   const user = JSON.parse(localStorage.getItem("user")!);
-  //   if (user !== null) {
-  //     setUser(user);
-  //   }
-  // }, []);
+  function isAuthenticated() {
+    return (
+      <>
+        <button
+          className="btn-login btn-hover"
+          type="button"
+          onClick={() => profile()}
+        >
+          Profile
+        </button>
+        <button
+          className="btn-login btn-hover"
+          type="button"
+          onClick={() => Logout()}
+        >
+          Log out
+        </button>
+      </>
+    );
+  }
 
-  // // @ts-ignore
-  // const email = user.email
-  // console.log(email)
+  function profile() {
+    navigate("/profile");
+    hadleClick();
+  }
 
+  function login() {
+    navigate("/login");
+    hadleClick();
+  }
 
-   function profile (){
-    navigate('/profile')
-    hadleClick() 
-    setIsLoggedIn(!isLoggedIn);
-
-   }
-
-   function login (){
-    navigate('/login')
-    hadleClick() 
-    setIsLoggedIn(!isLoggedIn);
-
-   }
   return (
     <div className="navbar">
       <div className="container">
         <div className="logo">
-          <img src={logo} alt="logo" onClick={() => navigate('/')} />
+          <img src={logo} alt="logo" onClick={() => navigate("/")} />
           <h1>BALKAN AI</h1>
         </div>
-       <div className={click ? "nav active" : "nav"}>
-        
-   { isLoggedIn ?( <><button className="btn-login btn-hover" type="button" onClick={() => login()}>Log in</button></>) :
-
-   ( <><button className="btn-login btn-hover" type="button" onClick={() => profile()}>Profile</button><button className="btn-login btn-hover" type="button" onClick={() => login()}>Log in</button></> )}
-
+        <div className={click ? "nav active" : "nav"}>
+          {isLoggedIn ? (
+            <>{isAuthenticated()}</>
+          ) : (
+            <>
+              <button
+                className="btn-login btn-hover"
+                type="button"
+                onClick={() => login()}
+              >
+                Log in
+              </button>
+            </>
+          )}
         </div>
         <div onClick={() => hadleClick()} className="hamburger">
           {click ? (
